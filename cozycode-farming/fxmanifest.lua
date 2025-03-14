0c10619cec3a26055cc4320cf08420fd16a067f9
Mist <114344332+MIST145@users.noreply.github.com>
2025-03-14 13:14:08 +0000

    Update fxmanifest.lua

--- cozycode-farming/fxmanifest.lua
+++ cozycode-farming/fxmanifest.lua
@@ -8,23 +8,34 @@ discord 'https://discord.gg/4cW6DR5q2U'
 
 shared_scripts {
 	"@ox_lib/init.lua",
+	'@es_extended/imports.lua',
 	"config.lua",
+	"bridge/esx/*.lua",
 	"bridge/qb/*.lua",
-	"modules/**/shared.lua",
+	"bridge/standalone/*.lua",
+	"modules/materials/shared.lua",
 	"core/shared.lua",
 	"locales/locale.lua",
 	"locales/translations/*.lua"
 }
 
 client_scripts {
+	"bridge/esx/client.lua",
 	"bridge/qb/client.lua",
+	"bridge/standalone/client.lua",
 	"modules/**/client.lua",
+	"modules/plants/client.lua",
+	"modules/seller/client.lua",
 	"core/client.lua"
 }
 
 server_scripts {
+	'bridge/esx/server.lua', -- Certifique-se de que este arquivo é carregado primeiro
 	"bridge/qb/server.lua",
+	"bridge/standalone/server.lua",
 	"modules/**/server.lua",
+	"modules/plants/server.lua",
+	"modules/seller/server.lua",
 	"core/server.lua"
 }
 
